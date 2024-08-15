72b6ca2817597e39a7d92f7aedcd5e19f55b09c1
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2024-08-15 19:44:19 +0300

    add close function in burger

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
-  $('.burger').click (function() {
+$(document).ready(function () {
+  $('.burger').click(function () {
     $('.js-burger-content').toggleClass('burger-content-open');
     $('.navigational-list').toggleClass('navigational-list-visible');
+  });
+
+	$(document).click(function (e) { 
+    var menu = $(".navigational-menu");
+    if (!menu.is(e.target) && menu.has(e.target).length === 0 ) {
+      $('.js-burger-content').removeClass('burger-content-open');
+      $('.navigational-list').removeClass('navigational-list-visible');
+    }
   });
 }); 
