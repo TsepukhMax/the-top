63946f1eec1aadcae9aee54392385f34b1280198
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2024-08-16 17:30:10 +0300

    fixed scroll top in submenu

--- script/script.js
+++ script/script.js
@@ -5,24 +5,21 @@ $(document).ready(function () {
   });
 
 	$(document).click(function (e) { 
-    var menu = $(".navigational-menu");
+    var menu = $('.navigational-menu');
     if (!menu.is(e.target) && menu.has(e.target).length === 0 ) {
       $('.js-burger-content').removeClass('burger-content-open');
       $('.navigational-list').removeClass('navigational-list-visible');
     }
   }); 
 
-$('.submenu a').click(function () {
-
-  let href = $(this).attr('href');
-
-  $('html, body').animate({
+$('.js-submenu-content').click(function () {
+  var href = $(this).attr('href');
+  $('html').animate({
       scrollTop: $(href).offset().top
   }, {
-      duration: 700, 
-      easing: "linear",
+      duration: 300, 
+      easing: 'linear',
   });
-
   return false;
 });
 });
