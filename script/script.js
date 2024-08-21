e8c4739f2bae2bb559698b03cf942970bb7102a3
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2024-08-21 19:41:53 +0300

    adapt to popup

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  /*-------Burger---------*/
   $('.burger').click(function () {
     $('.js-burger-content').toggleClass('burger-content-open');
     $('.navigational-list').toggleClass('navigational-list-visible');
@@ -11,7 +12,7 @@ $(document).ready(function () {
       $('.navigational-list').removeClass('navigational-list-visible');
     }
   }); 
-
+  /*------submenu---------*/
   $('.js-movie-link').click(function () {
     var href = $(this).attr('href');
 
@@ -27,14 +28,14 @@ $(document).ready(function () {
   });
 
 /*-------Popup---------*/
-  $('.button').click(function () {
+  $('.js-button').click(function () {
     var section = $(this).closest('section');
 
     var titleText = section.attr('data-title');
     var topText = section.attr('data-top');
 
-    $('.popup-title-text').text(titleText);
-    $('.popup-top').text(topText);
+    $('.js-popup-title-text').text(titleText);
+    $('.js-popup-top').text(topText);
 
     $('.popup').addClass('show');
     $('body').addClass('body-wrapper');
@@ -44,79 +45,12 @@ $(document).ready(function () {
     $('.popup').removeClass('show');
     $('body').removeClass('body-wrapper');
   });
+
+  $('.popup').click(function (e) { 
+    var popUp = $('.popup');
+    if (popUp.is(e.target)) {
+      $('.popup').removeClass('show');
+      $('body').removeClass('body-wrapper');
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// // Получаем элементы модального окна
-// var modal = document.getElementById("myModal");
-// var modalTitle = document.getElementById("modalTitle");
-// var modalText = document.getElementById("modalText");
-// var closeModal = document.getElementsByClassName("close")[0];
-
-// // Находим все кнопки, которые могут открыть модальное окно
-// var openModalBtns = document.querySelectorAll(".openModalBtn");
-
-// // Функция для открытия модального окна с динамическим контентом
-// openModalBtns.forEach(function(button) {
-//     button.addEventListener('click', function() {
-//         // Получаем данные из атрибутов data-title и data-content
-//         var title = this.getAttribute('data-title');
-//         var content = this.getAttribute('data-content');
-        
-//         // Устанавливаем текст заголовка и содержимого
-//         modalTitle.textContent = title;
-//         modalText.textContent = content;
-
-//         // Открываем модальное окно
-//         modal.style.display = "block";
-//     });
-// });
-
-// // Закрытие модального окна при нажатии на крестик
-// closeModal.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // Закрытие модального окна при клике вне его
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-// });
-
-
-  
-  /*------------Popup-----------*/
-//   var popupCall = $('[data-popup]');
-//   var popupClose = $('[data-close]');
-  
-//   popupCall.click(function (e) {
-//     e.preventDefault();
-
-//     var $this = $(this);
-//     var popupId = $this.data('popup');
-
-//     $(popupId).addClass('show');
-//     $('body').addClass('body-wrapper');
-//   });
-
-//   popupClose.click(function (e) {
-//     e.preventDefault();
-
-//     var $this = $(this);
-//     var popupParent = $this.parents('.popup');
-
-//     popupParent.removeClass('show');
-//     $('body').removeClass('body-wrapper');
-//   });
