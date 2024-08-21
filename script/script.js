a306924ee8ee02f9f233a471ce404d35817fc410
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2024-08-21 16:27:18 +0300

    fixed js in popup

--- script/script.js
+++ script/script.js
@@ -25,28 +25,98 @@ $(document).ready(function () {
     $('.navigational-list').removeClass('navigational-list-visible');
     return false;
   });
-  
-  /*------------Popup-----------*/
-  var popupCall = $('[data-popup]');
-  var popupClose = $('[data-close]');
-  
-  popupCall.click(function (e) {
-    e.preventDefault();
 
-    var $this = $(this);
-    var popupId = $this.data('popup');
+/*-------Popup---------*/
+  $('.button').click(function () {
+    var section = $(this).closest('section');
 
-    $(popupId).addClass('show');
+    var titleText = section.attr('data-title');
+    var topText = section.attr('data-top');
+
+    $('.popup-title-text').text(titleText);
+    $('.popup-top').text(topText);
+
+    $('.popup').addClass('show');
     $('body').addClass('body-wrapper');
   });
 
-  popupClose.click(function (e) {
-    e.preventDefault();
-
-    var $this = $(this);
-    var popupParent = $this.parents('.popup');
-
-    popupParent.removeClass('show');
+  $('.popup-close').click(function () {
+    $('.popup').removeClass('show');
     $('body').removeClass('body-wrapper');
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// // Получаем элементы модального окна
+// var modal = document.getElementById("myModal");
+// var modalTitle = document.getElementById("modalTitle");
+// var modalText = document.getElementById("modalText");
+// var closeModal = document.getElementsByClassName("close")[0];
+
+// // Находим все кнопки, которые могут открыть модальное окно
+// var openModalBtns = document.querySelectorAll(".openModalBtn");
+
+// // Функция для открытия модального окна с динамическим контентом
+// openModalBtns.forEach(function(button) {
+//     button.addEventListener('click', function() {
+//         // Получаем данные из атрибутов data-title и data-content
+//         var title = this.getAttribute('data-title');
+//         var content = this.getAttribute('data-content');
+        
+//         // Устанавливаем текст заголовка и содержимого
+//         modalTitle.textContent = title;
+//         modalText.textContent = content;
+
+//         // Открываем модальное окно
+//         modal.style.display = "block";
+//     });
+// });
+
+// // Закрытие модального окна при нажатии на крестик
+// closeModal.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // Закрытие модального окна при клике вне его
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+// });
+
+
+  
+  /*------------Popup-----------*/
+//   var popupCall = $('[data-popup]');
+//   var popupClose = $('[data-close]');
+  
+//   popupCall.click(function (e) {
+//     e.preventDefault();
+
+//     var $this = $(this);
+//     var popupId = $this.data('popup');
+
+//     $(popupId).addClass('show');
+//     $('body').addClass('body-wrapper');
+//   });
+
+//   popupClose.click(function (e) {
+//     e.preventDefault();
+
+//     var $this = $(this);
+//     var popupParent = $this.parents('.popup');
+
+//     popupParent.removeClass('show');
+//     $('body').removeClass('body-wrapper');
+//   });
