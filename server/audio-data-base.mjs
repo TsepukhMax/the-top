9e038f65ec2b920821bc7b10725fd41285484a64
Dmytro Serhatyi <dmytro.serhatyi@gmail.com>
2024-11-29 11:36:46 +0100

    added server

---
+++ server/audio-data-base.mjs
@@ -0,0 +1,42 @@
+export const audioDataBase = [
+  {
+    id: 29,
+    audioUrl: "audios/jurassic-park.ogg",
+  },
+  {
+    id: 72,
+    audioUrl: "audios/2001-a-space-odyssey.ogg",
+  },
+  {
+    id: 70,
+    audioUrl: "audios/o-brother-where-art-thou.ogg",
+  },
+  {
+    id: 74,
+    audioUrl: "audios/the-godfather.ogg",
+  },
+  {
+    id: 76,
+    audioUrl: "audios/the-lord-of-the-rings.ogg",
+  },
+  {
+    id: 98,
+    audioUrl: "audios/goodfellas.ogg",
+  },
+  {
+    id: 47,
+    audioUrl: "audios/star-wars-a-new-hope.ogg",
+  },
+  {
+    id: 23,
+    audioUrl: "audios/blade-runner.ogg",
+  },
+  {
+    id: 85,
+    audioUrl: "audios/guardinas-of-the-galaxy-vol-2.ogg",
+  },
+  {
+    id: 65,
+    audioUrl: "audios/baby-driver.ogg",
+  },
+];
